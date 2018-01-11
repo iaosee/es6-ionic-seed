4d519743676484f5b73e349d4d5ba0762f0607e0
oioio <xiao_f_x@163.com>
2018-01-11 19:15:37 +0800

    修改 Tools

--- app/modules/global/service/tools.service.js
+++ app/modules/global/service/tools.service.js
@@ -7,6 +7,21 @@ export default class ToolsService {
 
   }
 
+  functionHook() {
+
+    Function.prototype.before = function (fn) {
+      var _self = this;
+      return function () {
+        if (fn.apply(this, arguments) === false) return false;
+        return _self.apply(this, arguments);
+      }
+    }
+
+    func = func.before(function (response) {
+
+    });
+  }
+
   objKeysToLower(origin) {
     if (!origin) {
       return origin;
